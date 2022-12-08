29a15950df1586c0bccaccbfc0f465598cf66696
Raphael Serota <raphael.serota@gmail.com>
2022-12-08 13:32:02 -0600

    get balance

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
 const express = require('express');
 const app = express();
 app.use(express.json())
-const transactions = [];
+const points = [];
 
 
-app.post('/transactions', (req, res)=>{
+app.post('/points', (req, res)=>{
     console.log('body', req.body);
-    transactions.push(req.body);
+    req.body.remainingPoints = req.body.points
+    points.push(req.body);
 
     res.status(201).send();
 })
 
 // for setup before tests
-app.delete('/transactions', (req, res)=>{
-    transactions.length = 0;
+app.delete('/points', (req, res)=>{
+    points.length = 0;
     res.status(204).send();
 })
 
-app.get('/transactions', (req, res)=>{
-    console.log('transactions? ', transactions)
-    res.send(transactions)
+// for test assertions
+app.get('/points', (req, res)=>{
+    res.send(points)
+})
+
+app.get('/points/balance', (req, res)=>{
+    const balance = { }
+    for ( let transaction of points ) {
+        if ( !balance[transaction.payer] )  {
+            balance[transaction.payer] = transaction.remainingPoints
+        }
+        else {
+            balance[transaction.payer] += transaction.remainingPoints
+        }
+    }
+    res.send(balance)
 })
 
 app.listen(8080, ()=>{
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -4,22 +4,43 @@ const baseUrl = 'http://localhost:8080'
 
 describe('Transaction creation', ()=>{
     beforeAll(async ()=>{
-        await axios.delete(`${baseUrl}/transactions`)
+        await axios.delete(`${baseUrl}/points`)
     })
+
     describe('with valid inputs', ()=>{
         it('should return a 201 response', async ()=>{
             const testTransaction = { "payer": "DANNON", "points": 300, "timestamp": "2022-10-31T10:00:00Z" }
-            const postResponse = await axios.post(`${baseUrl}/transactions`, testTransaction)
+            const postResponse = await axios.post(`${baseUrl}/points`, testTransaction)
             expect(postResponse.status).toBe(201)
 
-            const getResponse = await axios.get(`${baseUrl}/transactions`)
-            console.log(getResponse.data)
+            const getResponse = await axios.get(`${baseUrl}/points`)
+            // console.log(getResponse.data)
         })
     })
+
     describe('with invalid inputs', ()=>{
         it('should return a 201 response', async ()=>{
-            const response = await axios.post(`${baseUrl}/transactions`)
+            const response = await axios.post(`${baseUrl}/points`)
             // console.log('resp ', response)
+        })
+    })
+})
+
+describe('Balance calculation', ()=>{
+    describe('with valid inputs', ()=>{
+        it('should return the balance', async ()=>{
+            await axios.delete(`${baseUrl}/points`)
+            await Promise.all([
+                axios.post(`${baseUrl}/points`,{ "payer": "DANNON", "points": 300, "timestamp": "2022-10-31T10:00:00Z" }),
+                axios.post(`${baseUrl}/points`, { "payer": "UNILEVER", "points": 200, "timestamp": "2022-10-31T11:00:00Z" }),
+                axios.post(`${baseUrl}/points`, { "payer": "DANNON", "points": -200, "timestamp": "2022-10-31T15:00:00Z" }),
+                axios.post(`${baseUrl}/points`, { "payer": "MILLER COORS", "points": 10000, "timestamp": "2022-11-01T14:00:00Z" }),
+                axios.post(`${baseUrl}/points`, { "payer": "DANNON", "points": 1000, "timestamp": "2022-11-02T14:00:00Z" })
+            ])
+
+            const response = await axios.get(`${baseUrl}/points/balance`)
+            console.log('response data', response.data)
+
         })
     })
 })
